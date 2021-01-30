da9a4cc1f5675b4bf841f52f675dc210b7d81e90
anhnbt-it <anhnbt.it@gmail.com>
2021-01-30 18:56:21 +0700

    Refactor code

--- src/app/books/book-edit/book-edit.component.ts
+++ src/app/books/book-edit/book-edit.component.ts
@@ -1,8 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { Book } from 'src/app/book';
 import { BookService } from 'src/app/book.service';
 
@@ -13,8 +11,6 @@ import { BookService } from 'src/app/book.service';
 })
 export class BookEditComponent implements OnInit {
 
-  book!: Book;
-
   bookForm = this.fb.group({
     id: null,
     title: [null, Validators.required],
@@ -24,39 +20,31 @@ export class BookEditComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private http: HttpClient,
     private route: ActivatedRoute,
     private bookService: BookService,
     private router: Router
     ) { }
 
-    ngOnInit(): void {
-      // First get the product id from the current route.
-      const routeParams = this.route.snapshot.paramMap;
-      const bookIdFromRoute = Number(routeParams.get('id'));
-  
-      // Find the product that correspond with the id provided in route.
-      // http://localhost:3000/books/1
-      this.http.get<Book>('http://localhost:3000/books/' + bookIdFromRoute).subscribe((data: Book) => {
-        this.book = data;
-        console.log(this.book);
-        this.bookForm = this.fb.group({
-          id: this.book.id,
-          title: [this.book.title, Validators.required],
-          author: [this.book.author, Validators.required],
-          description: [this.book.description, Validators.required],
-        });
-      });
-    }
-
-  onSubmit(): void {
-    this.updateBook(this.bookForm.value);
-    console.log("Update: ", this.updateBook(this.bookForm.value));
-    this.router.navigateByUrl("/");
+  ngOnInit(): void {
+    this.getBook();
   }
 
-  updateBook(book: Book): any {
-    return this.http.put<Book>('http://localhost:3000/books/' + book.id, book).subscribe((res: any) => {console.log(res)});
+  getBook(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const bookIdFromRoute = Number(routeParams.get('id'));
+
+    this.bookService.getBook(bookIdFromRoute).subscribe((data: Book) => {
+      this.bookForm = this.fb.group({
+        id: data.id,
+        title: [data.title, Validators.required],
+        author: [data.author, Validators.required],
+        description: [data.description, Validators.required],
+      });
+    });  }
+
+  onSubmit(): void {
+    this.bookService.updateBook(this.bookForm.value).subscribe(data => console.log(data));
+    this.router.navigateByUrl("/books");
   }
 
 }
